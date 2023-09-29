e87962a9ad496c4ebece52771643bd57466d2525
Nick Alteen <ncalteen@github.com>
2023-09-29 01:20:15 -0400

    CodeQL lab

--- scripts/vulnerable_script.py
+++ scripts/vulnerable_script.py
@@ -1,10 +1,12 @@
 """This script has intentional vulnerabilities for testing purposes."""
 
 
-# This function accepts an arbitrary regular expression and uses it to
-# search for a pattern in a string. It returns the first match object
-# found.
-def search(pattern, string):
-    import re
-
-    return re.search(pattern, string)
+# This function has a vulnerability.
+def vulnerable_function():
+    """This function has a vulnerability."""
+    # Vulnerability: CWE-78: Improper Neutralization of Special Elements used
+    # in an OS Command ('OS Command Injection')
+    # https://cwe.mitre.org/data/definitions/78.html
+    print("Enter your name: ")
+    name = input()
+    print("Hello, " + name)

e003a153072e09c0340b2fffb6fdb52258200fb3
Nick Alteen <ncalteen@github.com>
2023-09-29 01:23:37 -0400

    CodeQL lab

--- scripts/vulnerable_script.py
+++ scripts/vulnerable_script.py
@@ -1,12 +1,8 @@
 """This script has intentional vulnerabilities for testing purposes."""
 
 
-# This function has a vulnerability.
-def vulnerable_function():
-    """This function has a vulnerability."""
-    # Vulnerability: CWE-78: Improper Neutralization of Special Elements used
-    # in an OS Command ('OS Command Injection')
-    # https://cwe.mitre.org/data/definitions/78.html
-    print("Enter your name: ")
-    name = input()
-    print("Hello, " + name)
+# This prints an AWS secret key.
+def print_secret_key():
+    """This prints an AWS secret key."""
+    print("Your AWS secret key is: " + "AKIAIOSFODNN7EXAMPLE")
+    print("wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")

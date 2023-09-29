ba61bf804752ccb279d64537c0cf67ebe89d0088
Nick Alteen <ncalteen@github.com>
2023-09-29 01:26:41 -0400

    CodeQL lab

--- scripts/vulnerable_script.py
+++ scripts/vulnerable_script.py
@@ -1,8 +1,20 @@
 """This script has intentional vulnerabilities for testing purposes."""
 
+from flask import Flask
 
-# This prints an AWS secret key.
-def print_secret_key():
-    """This prints an AWS secret key."""
-    print("Your AWS secret key is: " + "AKIAIOSFODNN7EXAMPLE")
-    print("wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
+app = Flask(__name__)
+
+
+import traceback
+
+
+def do_computation():
+    raise Exception("Secret info")
+
+
+# BAD
+def server_bad():
+    try:
+        do_computation()
+    except Exception as e:
+        return traceback.format_exc()
